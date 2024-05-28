f9d063ce3c634f5ccb2748ab067ca47b631ddb39
Devendra Moharir <150278433+Devendra412@users.noreply.github.com>
2024-05-28 14:42:36 +0530

    Update DAY6-Reeborg's Problems.py

--- DAY6-Reeborg's Problems.py
+++ DAY6-Reeborg's Problems.py
@@ -10,7 +10,7 @@
 # In[ ]:
 
 
-#HURDLE & MAZE PROBLEMS
+#HURDLE PROBLEM
 def turn_right():
     turn_left()
     turn_left()
@@ -19,6 +19,25 @@ def turn_right():
 while front_is_clear():
     move()
            
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear() and wall_on_right():
+        move()
+    else:
+        turn_left()
+
+#MAZE PROBLEM
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
 while not at_goal():
     if right_is_clear():
         turn_right()
