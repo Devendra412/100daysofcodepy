ee17fee82e72ad86e6d03cfdbf2ba894d5057d93
Devendra Moharir <150278433+Devendra412@users.noreply.github.com>
2024-05-28 13:29:43 +0530

    Add files via upload

---
+++ DAY6-Reeborg's Problems.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# DAY 7
+# Reeborg's World Path Problems
+# 
+# This is a Reeborg world problem.
+# Go to:  https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# In[ ]:
+
+
+#HURDLE & MAZE PROBLEMS
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+           
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear() and wall_on_right():
+        move()
+    else:
+        turn_left()
+
